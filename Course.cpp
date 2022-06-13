afe4a18c487b88606985d922bca6c0db52c661d8
yazdan bahadori <yzdanbhadry@gmail.com>
2022-06-13 23:09:27 +0430

    add class Person

--- Course.cpp
+++ Course.cpp
@@ -96,7 +96,167 @@ class Course {
 
     };
 
+class Person {
 
+private:
+    string firstName;
+    string lastName;
+    string id;
+    double  workHour;
+    int year;
+
+
+public:
+
+    ~Person() {
+    }
+
+
+    Person(
+            string firstName,
+            string lastName,
+            string id,
+            double workHour ,
+            int year
+    ) {
+        this->firstName = firstName;
+        this->lastName = lastName;
+        this->id = id;
+        this->workHour = workHour;
+        this->year = year;
+
+        if (validate() == false) {
+            exit(0);
+        }
+
+    }
+
+    // Copy Constructer
+    Person(const Person& constOld)
+    {
+        this->firstName = constOld.firstName;
+        this->lastName = constOld.lastName;
+        this->id = constOld.id;
+        this->workHour = constOld.workHour;
+        this->year = constOld.year;
+    }
+
+    void setFirstName(string name)
+    {
+        this->firstName = name;
+    }
+
+    void setLastName(string last)
+    {
+        this->lastName = lastName;
+    }
+
+    void setID(string id) {
+        this->id = id;
+    }
+
+
+    void setWorkHour(double workHour)
+    {
+        this->workHour = workHour;
+    }
+
+    void setYear(int year)
+    {
+        this->year = year;
+    }
+
+
+    string getFirstName()
+    {
+        return this->firstName;
+    }
+
+    string getLastName()
+    {
+        return this->lastName;
+    }
+
+    string getId()
+    {
+        return this->id;
+    }
+
+    double getWorkHour()
+    {
+        return this->workHour;
+    }
+
+    int getYear()
+    {
+        return year;
+    }
+
+    bool validate()
+    {
+
+        if (id.length() > 8 && id.length() < 11)
+        {
+            int testOne = stoi(id.substr(0, 2));
+            if (testOne <= 84 && testOne >= 0)
+            {
+                bool testTwo = is_number(id.substr(3, 5));
+                if (testTwo == false)
+                {
+                    int testThree;
+
+                    for (int i = 5; i < id.length(); i++)
+                    {
+                        int temp = stoi(id.substr(i, i + 1));
+                        if (temp < 4 || temp > 6) {}
+                        else
+                        {
+                            cout << "Invalid ID";
+                            return false;
+                            break;
+                        }
+                    }
+
+                }
+                else { cout << "Invalid ID" << endl;  return false; };
+            }
+            else
+            {
+                cout << "Invalid ID" << endl; return false;
+            }
+        }
+        else
+        {
+            cout << "Invalid ID" << endl;
+
+            return false;
+        }
+
+        return true;
+
+    }
+
+    double calculateSalary()
+    {
+        int a = static_cast<int>(workHour);
+        return a * 100000;
+    }
+
+
+    friend ostream& operator<<(ostream& output, const Person& person)
+    {
+        output << "Salam";
+        return output;
+    }
+
+    friend istream& operator>>(istream& input, Person& person)
+    {
+        input >> person.firstName >> person.lastName >> person.id;
+        return input;
+    }
+
+
+};
 
 
 
